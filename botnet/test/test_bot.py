3ddf3c5df59769bc06f8bc1e9cfd222c517f7a6a
Victor Hugo Germano <victorhg@gmail.com>
2010-04-01 15:13:54 +0100

    Added bot test

---
+++ botnet/test/test_bot.py
@@ -0,0 +1,19 @@
+'''
+Created on Apr 1, 2010
+
+@author: victorhg
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def test_weather_command(self):
+        bot = BotnetJabberClient()
+        bot.weather("", ["Dublin"])
+        
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_weather_command']
+    unittest.main()
