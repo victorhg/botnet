3f0a3636b33d12a165231dca427b8d499f62f64a
Victor Hugo Germano <victorhg@gmail.com>
2010-04-08 00:43:19 +0100

    Changed unit for weather report to Celcius

--- botnet/finance.py
+++ botnet/finance.py
@@ -7,11 +7,12 @@ import urllib
 
 DEFAULT_UNITARY_CURRENCY = "USD"
 CURRENCIES = ["EUR", "GBP", "JPY", "BRL", "AUD", "CAD"]
+API_URL = "http://www.exchangerate-api.com/%s/%s/%f"
 
 def exchange_rate():
     result = "Exchange Rates provided by http://www.travelground.com/ \n\n"
     for currency in CURRENCIES:
-        url = urllib.urlopen("http://www.exchangerate-api.com/%s/%s/%f"%(DEFAULT_UNITARY_CURRENCY,currency,1))
+        url = urllib.urlopen(API_URL%(DEFAULT_UNITARY_CURRENCY,currency,1))
         exchange_rate = url.read()
         result = result + ("%s/%s - %s \n"%(DEFAULT_UNITARY_CURRENCY, currency, exchange_rate ))
     return result
